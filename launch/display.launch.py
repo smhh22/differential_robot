7dd36d9c6f64397eae73a38091b8c13a8520fdc4
smhh22 <s.m.m.m.h2013@gmail.com>
2025-11-18 13:13:33 +0330

    refacctor launch file

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -17,18 +17,10 @@ def generate_launch_description():
         executable='robot_state_publisher',
         parameters=[{'robot_description': Command(['cat ', LaunchConfiguration('model')])}]
     )
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{'robot_description': Command(['cat ', default_model_path])}],
-        condition=UnlessCondition(LaunchConfiguration('gui'))
-    )
     joint_state_publisher_gui_node = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui',
-        condition=IfCondition(LaunchConfiguration('gui'))
     )
     rviz_node = Node(
         package='rviz2',
@@ -39,10 +31,8 @@ def generate_launch_description():
     )
     
     return LaunchDescription([
-        DeclareLaunchArgument(name='gui', default_value='True', description='Flag to enable joint_state_publisher_gui'),
         DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot model file'),
         DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
-        joint_state_publisher_node,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node, 
